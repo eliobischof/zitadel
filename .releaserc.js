230bd51744431533b6bf19138081caa6f8ae8bd8
Elio Bischof <elio@zitadel.com>
2024-04-03 20:59:56 +0200

    fix: cfg

--- .releaserc.js
+++ .releaserc.js
@@ -2,13 +2,13 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
-    { name: "v1.3.x", range: "1.3.x", channel: "1.3.x" },
+    { name: "v1.3.x", channel: "1.3.x" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
 //    "@semantic-release/release-notes-generator",
     [
-      "@semantic-release/github", { "draftRelease": true }
+      "@semantic-release/github", { "draft": true }
     ],
   ],
 };
