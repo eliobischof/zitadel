681ca0ac5f82365d96778da087776f6a9a6b78da
Elio Bischof <elio@zitadel.com>
2024-04-03 21:15:53 +0200

    range

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
-    { name: "v1.3.x", channel: "1.3.x" },
+    { name: "v1.3.x", range: "1.3.x", channel: "1.3.x" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
