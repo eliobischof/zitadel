3d0b8efa2a5da01561ae6fad205534848f6e3884
Elio Bischof <elio@zitadel.com>
2024-04-03 22:50:39 +0200

    fix: publish

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,6 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
-    { name: "v1.3.x", range: "1.3.x", channel: "1.3.x" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
